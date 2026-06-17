845e4db14a055ceeeef880cd0e9d3d1c5a30f0de
emikka3 <mikaelacapela@gmail.com>
2026-06-16 21:17:14 -0300

    feat: implementacao de fundo e campos de texto

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,8 +5,7 @@ interface InputProps {
 export default function Input({ placeholder }: InputProps) {
   return (
     <input
-      type="text"
-      className="w-full border border-gray-300 rounded-full px-4 py-2 bg-transparent text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-white"
+      className="bg-[#eaf6ff] text-white placeholder-gray-300 px-4 py-2  gap-8 rounded-full outline-none border-none outline-none focus:outline-none focus:ring-0"
       placeholder={placeholder}
     />
   );
--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -3,13 +3,11 @@ import Input from "../../components/Input";
 function Cadastro() {
   return (
     <section className="w-full h-dvh bg-[#2a2a72] flex items-center justify-center overflow-hidden relative">
-      {/* Losango amarelo */}
-      <div className="absolute w-[600px] h-[600px] bg-[#FFA400] rotate-45"></div>
+      <div className="absolute w-[200px] h-[200px] bg-[#FFA400] rotate-45"></div>
 
-      {/* Conteúdo centralizado */}
-      <div className="relative z-10 flex flex-col items-center gap-4 w-[300px]">
+      <div className="relative z-60 flex flex-col items-center gap-4 w-[300px]">
         <img
-          className="w-20 h-auto"
+          className="w-16 h-auto max-w-[80px]"
           alt="Logo EPOL"
           src="/img/LogoEPOLPreta.png"
         />
@@ -19,10 +17,14 @@ function Cadastro() {
         </p>
 
         <Input placeholder="Email" />
+
         <Input placeholder="Nome Completo" />
+
         <Input placeholder="Nome do Usuário" />
 
-        <button className="w-full rounded-full bg-[#FFA400] text-white font-bold py-2 text-[15px]">
+        <Input placeholder="Senha" />
+
+        <button className="w-full rounded-full bg-[#FFA400] text-white font-bold py-2 text-[15px] rounded-full outline-none border-none outline-none focus:outline-none focus:ring-0">
           CADASTRAR
         </button>
 
