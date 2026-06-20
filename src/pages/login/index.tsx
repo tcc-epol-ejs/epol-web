9cabef0d56de816a32e330bfa750df916c912fe4
Ricardo <ricardogit17@gmail.com>
2026-06-20 12:39:00 -0300

    refactor: padronização dos componentes e adição das imagens de formas geométricas decorativas na tela

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,30 +1,52 @@
-import Input from "../../components/input";
+import Textbox from "../../components/textboxes/textbox";
+import Botao from "../../components/botoes/botao";
 
 export default function Login() {
   return (
     <div>
       <section className="min-h-screen w-full bg-[#e8f0fe] flex flex-col items-center overflow-hidden justify-center relative">
-        <div className="flex flex-col items-center gap-4">
-          <img
-            className="w-50 h-auto"
-            alt="Logo EPOL"
-            src="/LogoEPOLPreta.png"
-          />
-          <h1 className="text-[#2d2d6b] font-bold text-6xl tracking-wide mb-0.5">
-            SEJA BEM VINDO!!
-          </h1>
-          <Input placeholder="Nome de usuário ou Email" />
-          <Input placeholder="Senha" />
-          <button className="w-100 py-5 rounded-full bg-[#2d2d6b] text-white tracking-widest mt-3 text-2xl">
-            CONTINUAR
-          </button>
-          <p className="text-[#2d2d6b] text-2xl">Esqueceu a senha?</p>
-          <p className="text-[#7b6bb5] text-2xl">
-            Não tem uma conta?&nbsp;
-            <span className="text-[#2d2d8b] font-bold cursor-pointer">
-              Cadastre-se
-            </span>
-          </p>
+        <div className="h-screen w-screen flex items-center justify-center p-5 bg-[#eaf6ff]">
+          <div className="h-screen w-screen flex items-center justify-center">
+            <img
+              src="/efeitotras.png"
+              alt="efeito traseiro"
+              className="absolute inset-0 w-full h-full object-cover z-0"
+            />
+            <img
+              src="/losango.png"
+              alt="losango"
+              className="absolute inset-0 w-full h-full object-contain z-0"
+            />
+
+            <div className="flex flex-col items-center gap-4 z-10 relative">
+              <img
+                className="w-50 h-auto"
+                alt="Logo EPOL"
+                src="/LogoEPOLPreta.png"
+              />
+              <h1 className="text-[#2d2d6b] font-bold text-6xl tracking-wide mb-0.5">
+                SEJA BEM VINDO!!
+              </h1>
+              <Textbox placeholder="Nome de usuário ou Email" />
+              <Textbox placeholder="Senha" />
+              <Botao bgColor="#2d2d6b" textColor="#ffffff">
+                CONTINUAR
+              </Botao>
+              <p className="text-[#2d2d6b] text-2xl">Esqueceu a senha?</p>
+              <p className="text-[#7b6bb5] text-2xl">
+                Não tem uma conta?&nbsp;
+                <span className="text-[#2d2d8b] font-bold cursor-pointer">
+                  Cadastre-se
+                </span>
+              </p>
+            </div>
+
+            <img
+              src="/efeitofrente.png"
+              alt="efeito frente"
+              className="absolute inset-0 w-full h-full object-cover z-0"
+            />
+          </div>
         </div>
       </section>
 
@@ -72,11 +94,6 @@ export default function Login() {
           <p className="text-2xl mt-3 whitespace-nowrap text-gray-300">
             Título de Eleitor
           </p>
-        </div>
-        <div className="w-full border-t border-gray-500 mt-10 pt-4 flex gap-6 text-gray-400 text-sm">
-          <span>© 2026 EPOL</span>
-          <span>Trabalho de Conclusão de Curso</span>
-          <span>Uso não comercial</span>
         </div>
       </footer>
     </div>
